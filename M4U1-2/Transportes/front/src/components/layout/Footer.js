316c311c8bd0c9cc0ff6cc685988c8ae6c72d85d
Agustin Chipy <agustinchipy@gmail.com>
2024-10-07 20:02:50 -0300

    Union M4U1-2 y Tarea React Parte 2

--- M4U1-2/Transportes/front/src/components/layout/Footer.js
+++ M4U1-2/Transportes/front/src/components/layout/Footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import '../../styles/components/layouts/Footer.css';
 
 const Footer = (props) => {
     return (
--- M4U1-2/Transportes/front/src/components/layout/Header.js
+++ M4U1-2/Transportes/front/src/components/layout/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import '../../styles/components/layouts/Header.css';
 
 const Header = (props) => {
     return (
